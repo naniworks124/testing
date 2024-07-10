11f1a9691777bb43c5f6caf9c0807de9f2eb5638
naniworks124 <131702112+naniworks124@users.noreply.github.com>
2024-07-10 06:31:33 +0530

    Update wzml_futuristic.py
    Signed-off-by: naniworks124 <131702112+naniworks124@users.noreply.github.com>

--- bot/helper/themes/wzml_futuristic.py
+++ bot/helper/themes/wzml_futuristic.py
@@ -1,6 +1,6 @@
 class WZMLStyle: # Don't Change This
     ST_BN1_NAME = 'Owner'
-    ST_BN1_URL = 'https://t.me/nitinjack'
+    ST_BN1_URL = 'https://t.me/hybridjunior'
     ST_BN2_NAME = 'Channel'
     ST_BN2_URL = 'https://t.me/huntnetwork'
     ST_MSG = '''<i>This Bot Is only for Authorised users</i>'''
