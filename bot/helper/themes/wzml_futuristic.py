f33c3aa209563603ddf189814f4de3215759046a
naniworks124 <131702112+naniworks124@users.noreply.github.com>
2024-07-10 06:31:00 +0530

    Add files via upload
    Signed-off-by: naniworks124 <131702112+naniworks124@users.noreply.github.com>

---
+++ bot/helper/themes/wzml_futuristic.py
@@ -0,0 +1,7 @@
+class WZMLStyle: # Don't Change This
+    ST_BN1_NAME = 'Owner'
+    ST_BN1_URL = 'https://t.me/nitinjack'
+    ST_BN2_NAME = 'Channel'
+    ST_BN2_URL = 'https://t.me/huntnetwork'
+    ST_MSG = '''<i>This Bot Is only for Authorised users</i>'''
+    ST_UNAUTH = '''<i>You Are not authorized user! Please Contact Owner to get Access</i>'''
